ddfad8876970f2dc85a587034dc9b43f9cb03534
John Pearson <jmxpearson@gmail.com>
2015-11-25 08:15:37 -0500

    added bare-bones expectation operator on nodes

--- vbgraph.jl
+++ vbgraph.jl
@@ -14,6 +14,7 @@ abstract Factor
 "Defines a node (variable) in the model."
 abstract Node
 
+"Node corresponding to a collection of random variables."
 type RandomNode{D <: Distribution, N} <: Node
     data::Array{D, N}
     factors::Vector{Factor}
@@ -21,3 +22,7 @@ end
 
 RandomNode{D <: Distribution}(d::Type{D}, pars...) = RandomNode(map(d, pars...), Factor[])
 
+# define an expectation method on Nodes
+"Calculates the expected value of a Node x."
+E(x::Node) = x
+E(x::RandomNode) = map(mean, x.data)
