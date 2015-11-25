49d71649f4e1db75eed95b477ac578fc9a317c36
John Pearson <jmxpearson@gmail.com>
2015-11-25 10:53:43 -0500

    basic factor assembly working
    - defined Union type AnyNode that's a union over concrete subtypes of
      Node
    - defined conversion from Array to ConstantNode
    - used this to allow for factor instatiation

--- VB.jl
+++ VB.jl
@@ -1,15 +1,18 @@
-module VB
-@doc """
+"""
 A Variational Bayesian modeling approach. Models are defined by 
 bipartite graphs in which factors defining terms in the variational
 objective are connected to nodes defining variables.
-""" -> current_module()
+"""
+module VB
 
 using Distributions
+import Base.convert
 
 # data types, including VBModel, Factor, and Node
 include("types.jl")
 
-export VBModel, Factor, Node, RandomNode, E
+export VBModel, Factor, Node, RandomNode, ConstantNode,
+    E, Elog, var,
+    LogNormalFactor, value
 
 end  # module
--- types.jl
+++ types.jl
@@ -13,10 +13,39 @@ type RandomNode{D <: Distribution, N} <: Node
     data::Array{D, N}
     factors::Vector{Factor}
 end
-
 RandomNode{D <: Distribution}(d::Type{D}, pars...) = RandomNode(map(d, pars...), Factor[])
+
+type ConstantNode{T <: Number, N} <: Node
+    data::Array{T, N}
+    factors::Vector{Factor}
+end
+
+typealias AnyNode Union{ConstantNode, RandomNode}
+convert(::Type{AnyNode}, x::Array) = ConstantNode(x, Factor[])
+
+# bind a factor and a node in the graph
+bind(n::Node, f::Factor) = push!(n.factors, f)
 
 # define an expectation method on Nodes
 "Calculates the expected value of a Node x."
 E(x::Node) = x
 E(x::RandomNode) = map(mean, x.data)
+E(x::ConstantNode) = x
+
+"Calculates the variance of a Node x."
+var(x::RandomNode) = map(var, x.data)
+var(x::ConstantNode) = zeros(x.data)
+
+"Calculates the expected value of the log of a Node x."
+Elog(x::ConstantNode) = map(log, x.data)
+
+
+# define some factors
+type LogNormalFactor 
+    x::AnyNode
+    μ::AnyNode
+    τ::AnyNode
+end
+
+value(f::LogNormalFactor) = -(1/2) * (E(f.τ) .* ( var(f.x) + var(f.mu) + (E(f.x) - E(f.μ))^2 ) + log(2π) + Elog(τ))
+
